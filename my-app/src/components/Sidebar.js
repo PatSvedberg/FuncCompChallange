445117b8777cf0689aaad467a43201d682345054
PatSvedberg <pat.svedberg@gmail.com>
2023-06-01 14:14:45 +0000

    Reach challange part 2

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
 const Sidebar = () => {
     return (
-        <div>
+        < div className = {css.sidebar} >
             <a>MyPhotos</a>
             <a>My Illustations</a>
             <a>My Paintings</a>
